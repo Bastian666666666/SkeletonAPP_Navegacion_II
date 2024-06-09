df3bd65dde216a96dbf7b0ebc41ce54a8ea17f33
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-08 21:49:57 -0400

    Agregando items a components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+//Agrego el components certificaciones para que se pueda navegar a el
+import { CertificacionesComponent } from './components/certificaciones/certificaciones.component';
+
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+
+  //Agrego la ruta a certificaciones
+  {
+    path: 'certificaciones', 
+    component: CertificacionesComponent
   },
   {
     path: '',
