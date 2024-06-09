f958ee37442a2650ac0b6eaffacbc047ad7ae704
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-08 23:57:39 -0400

    rutas componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-//Agrego el components certificaciones para que se pueda navegar a el
+//1.1 Agrego el components certificaciones para que se pueda navegar a el
 import { CertificacionesComponent } from './components/certificaciones/certificaciones.component';
+
+//1.2 Agrego el components certificaciones para que se pueda navegar a el
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+
+//1.2Agrego el components certificaciones para que se pueda navegar a el
+import { MisDatosComponent } from './components/mis-datos/mis-datos.component';
 
 const routes: Routes = [
   {
@@ -10,16 +16,38 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
 
-  //Agrego la ruta a certificaciones
+  //1.1 Agrego la ruta a certificaciones como component y le llamo certificacionesComponent 
   {
     path: 'certificaciones', 
     component: CertificacionesComponent
   },
+
+  //1.2 Agrego la ruta a experiencia-laboral como component y le llamo ExperienciaLaboralComponent 
+
+  {
+    path: 'experiencia-laboral', 
+    component: ExperienciaLaboralComponent
+  },
+
+    //1.3 Agrego la ruta a experiencia-laboral como component y le llamo ExperienciaLaboralComponent 
+  {
+    path: 'mis-datos', 
+    component: MisDatosComponent
+  },
+
+  // Esta es para cuando sea en blanco
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'not-found',
+    loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
+
+  //! Falta una para cuando no se encuentre la ruta 404
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// 2.1 Importe CertificacionesComponent
+import { CertificacionesComponent } from './components/certificaciones/certificaciones.component'; 
+
+// 2.2 Importe ExperienciaLaboralComponent
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component'; 
+
+// 2.3 Importe MisDatosComponent
+import { MisDatosComponent } from './components/mis-datos/mis-datos.component'; 
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, 
+                CertificacionesComponent, // 2.1 CertificacionesComponent sin esto no dejaba usar elementos de ionic
+                ExperienciaLaboralComponent, //2.2 ExperienciaLaboralComponent 
+                MisDatosComponent //2.3 MisDatosComponent
+              ], 
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
